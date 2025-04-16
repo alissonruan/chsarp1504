cf28531a088e02ddcf796c34bb9727c25f33fee1
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-04-15 22:26:34 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,65 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vetores
+namespace ca1504
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   /*
-            int tamanho = 5;
-            int i;
-            int[] lista = new int[5];
-            lista[0] = 10;
-            lista[1] = 20;
-            lista[2] = 30;
-            lista[3] = 2;
-            lista[4] = 4;
-
-            for (i = 0; i < tamanho; i++)
-            {
-            Console.WriteLine("Valor: {0}", lista[0]);
-            }*/
-
-            /*int i;
-            int tamanho = 5;
-            int[] lista = new int[tamanho];
-
-            for (i = 0; i < tamanho; i++)
-            {
-                Console.WriteLine("Digite um valor: ");
-                lista[i] = int.Parse(Console.ReadLine());
-            }
-            for (i = 0; i < tamanho; i++)
-            {
-                Console.WriteLine("Valor: {0}", lista[0]);
-            }*/
-
-            /*
-            Console.WriteLine("Digitie um valor: ");
-            i = int.Parse(Console.ReadLine());
-
-            if (i == lista.Length)
-            {
-                Console.WriteLine("O valor está na posição: " + lista);
-                Console.ReadLine();
-            } else
-            {
-                Console.WriteLine("Valor inválido");
-                Console.ReadLine();
-
-            }*/
-
-            Carro fusca = new Carro();
-
-
-            fusca.rodas[0] = new Roda();
-            
-            /*fusca.fd.aro = 17;
-            fusca.fe.aro = 17;
-            fusca.fe.material = "aluminio";
-            fusca.fd.material = "aluminio";*/
-
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "tio";
+            pessoa.Email = "teste@teste.com";
+            Console.WriteLine("Nome: {0}", pessoa.Nome);
+            Console.WriteLine("Email: {0}", pessoa.Email);
+            Cliente c = new Cliente();
+            c.cadastrar();
+            pessoa.cadastrar();
         }
     }
 }
